edb32b8d8be7dab2c68fc7c63b3a7bb97e2044a7
khalid almuhawis <khalidalmuhawis0@gmail.com>
2020-08-25 15:09:12 +0300

    queue (last solution)

--- queue.py
+++ queue.py
@@ -1,79 +1,83 @@
 class Node:
-    def __init__(self, data, next=None):
+    def __init__(self,data,next_node = None):
         self.data = data
-        self.next = next
+        self.next_node = next_node
+
+    def set_next_node(self, next_node):
+        self.next_node = next_node
+
+    def get_next_node(self):
+        return self.next_node
 
     def get_data(self):
         return self.data
 
-    def get_next(self):
-        return self.next
-
-    def set_next(self, node):
-        self.next = node
-
 
 class Queue:
-    def __init__(self, limit=10, front=None, back=None):
-        self.front = front
-        self.back = back
-        self.limit = limit
+    def __init__(self):
+        self.front_node = None
+        self.back_node = None
         self.length = 0
 
-
-    def is_full(self):
-        return self.length == self.limit
-
     def peek(self):
-        return self.front
+        if self.is_empty():
+            print("empty queue")
+        else:
+            return self.front_node.get_data()
+
+    def checked_en(self, data):
+        new_node = Node(data)
+        if self.is_empty():
+            self.front_node = new_node
+            self.back_node = new_node
+        else:
+            self.back_node.set_next_node(new_node)
+            self.back_node = new_node
+        self.length += 1
+
+    def enqueue(self, data):
+        if data > 12:
+            my_data = data
+            counter = 1
+            while my_data > 12:
+                my_data -= 12
+                counter += 1
+            while counter != 1:
+                self.checked_en(12)
+                counter -= 1
+            if my_data != 0:
+                self.checked_en(my_data)
+        else:
+            self.checked_en(data)
+
+    def dequeue(self):
+        if self.is_empty():
+            print("empty queue")
+        else:
+            removed_node = self.front_node
+            if self.get_length == 1:
+                self.front_node = None
+                self.back_node = None
+            else:
+                self.front_node = removed_node.get_next_node()
+            self.length -= 1
+            return removed_node.get_data()
 
     def is_empty(self):
         return self.length == 0
 
-    def enqueue(self, data):
-        if self.is_full():
-            print("no space!")
-        else:
-            new_node = Node(data)
-            if self.is_empty():
-                self.front = new_node
-            else:
-                self.back.set_next(new_node)
-            self.back = new_node
-            self.length += 1
+    def get_length(self):
+        return self.length
 
-    def dequeue(self):
-        if self.is_empty():
-            return "Queue is empty"
-        else:
-            removed_node = self.front
-            self.front = removed_node.get_next()
-            self.length -= 1
-            return removed_node.get_data()
+    def get_waiting_time(self):
+        return self.length*0.5
 
-
-
-first_room = Queue()
-
-first_room.enqueue("khalid")
-first_room.enqueue("ahmed")
-first_room.enqueue("mohammed")
-first_room.enqueue("gfhsgh")
-first_room.enqueue("dfgdgh")
-first_room.enqueue("njjkdgtj")
-first_room.enqueue("sfjfrhj")
-first_room.enqueue("jyutkyukuk")
-first_room.enqueue("nj")
-first_room.enqueue("sfjfrh")
-first_room.enqueue("jyutkyuku")
-
-
-print(first_room.dequeue())
-print(first_room.dequeue())
-print(first_room.dequeue())
-print(first_room.dequeue())
-print(first_room.dequeue())
-print(first_room.dequeue())
-print(first_room.dequeue())
-print(first_room.dequeue())
-print(first_room.dequeue())
+park_queue = Queue()
+print("waiting time: %s"%(park_queue.get_waiting_time()))
+park_queue.enqueue(16)
+park_queue.enqueue(12)
+park_queue.enqueue(8)
+park_queue.enqueue(7)
+print("waiting time: %s"%(park_queue.get_waiting_time()))
+print("the group of %s come in"%(park_queue.dequeue()))
+print("waiting time: %s"%(park_queue.get_waiting_time()))
